fcca14ab028d05aaf433154c89ded54cceda1009
Craig Rhodes <cdr2139@columbia.edu>
2016-10-28 19:56:24 -0400

    Initial commit.

--- MyProject/ViewController.swift
+++ MyProject/ViewController.swift
@@ -10,16 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var hiLabel: UILabel!
+    @IBOutlet weak var byeLabel: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
 
 }
 
